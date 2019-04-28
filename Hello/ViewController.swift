c988de393e757a64a7c77f131176b0c8c5939218
ryo.s <aqua.timez.saiko@gmail.com>
2019-04-28 21:00:16 +0900

    Initial Commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by ryo shirai on 2019/04/28.
+//  Copyright © 2019 ryo shirai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
